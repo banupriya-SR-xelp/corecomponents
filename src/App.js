2da0344cd631255701af538b33cf2f89ee4000fc
subash-v <subash.v@xelpmoc.in>
2019-11-15 10:27:07 +0530

    added signUp and Login section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
 import Input from './component/general/Input';
+import { Route, Switch, Redirect } from "react-router-dom";
+import Auth from './component/Auth/Auth';
+import NotFound from './component/general/NotFound';
 
 function App() {
   return (
-    <div className={styles.App}>
-    <Input/>
-    </div>
+    <Switch>
+    <Route exact path="/" component={Auth} />
+  
+    <Route exact path="/notfound" component={NotFound} />
+    <Route>
+      <Redirect to="/notfound"></Redirect>
+    </Route>
+  </Switch>
   );
 }
 
--- src/component/Auth/Auth.js
+++ src/component/Auth/Auth.js
@@ -1,11 +1,100 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styles from "./Auth.css";
+import Input from "../general/Input";
 
 export default class Auth extends Component {
-    render() {
-        return (
-            <div>
-                SignUp
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailValue: "",
+      password: "",
+      confirmPassword: ""
+    };
+  }
+  onChangeEmail = emailValue => {
+    this.setState({
+      emailValue
+    });
+  };
+  onChangePassword = password => {
+    this.setState({
+      password
+    });
+  };
+  onChangeConfirmPassword = confirmPassword => {
+    this.setState({
+      confirmPassword
+    });
+  };
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.signUpBase}>
+          <div className={styles.container}>
+            <div className={styles.formConatiner}>
+              <div className={styles.signUp}>SignUp</div>
+              <div className={styles.inputContainer}>
+                <Input
+                  placeholder={"Email"}
+                  value={this.state.emailValue}
+                  onChange={val => this.onChangeEmail(val)}
+                  fontSize={14}
+                  maxLength={240}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <Input
+                  type={"password"}
+                  placeholder={"Password"}
+                  value={this.state.password}
+                  onChange={val => this.onChangePassword(val)}
+                  fontSize={14}
+                  maxLength={240}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <Input
+                  type={"password"}
+                  placeholder={"Confirm Password"}
+                  value={this.state.confirmPassword}
+                  onChange={val => this.onChangeConfirmPassword(val)}
+                  fontSize={14}
+                  maxLength={240}
+                />
+              </div>
+              <div className={styles.button}>signUp</div>
             </div>
-        )
-    }
+          </div>
+        </div>
+        <div className={styles.loginBase}>
+          <div className={styles.container}>
+            {" "}
+            <div className={styles.formConatiner}>
+              <div className={styles.signUp}>Login</div>
+              <div className={styles.inputContainer}>
+                <Input
+                  placeholder={"Email"}
+                  value={this.state.emailValue}
+                  onChange={val => this.onChangeEmail(val)}
+                  fontSize={14}
+                  maxLength={240}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <Input
+                  type={"password"}
+                  placeholder={"Confirm Password"}
+                  value={this.state.confirmPassword}
+                  onChange={val => this.onChangeConfirmPassword(val)}
+                  fontSize={14}
+                  maxLength={240}
+                />
+              </div>
+              <div className={styles.button}>Login</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
