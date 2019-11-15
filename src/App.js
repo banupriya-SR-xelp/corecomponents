aa4cb7d177c6e154b42f4d9dfc48551451941ca4
banupriya <banupriya@xelpmoc.in>
2019-11-15 09:42:04 +0530

    bordered button component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import BorderdButton from "./component/BorderdButton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BorderdButton
+        name={"Submit"}
+        backgroundColor={"#d3d3d3"}
+        fontcolor={"blue"}
+        width={100}
+        height={30}
+      />
     </div>
   );
 }
--- src/component/BorderdButton.js
+++ src/component/BorderdButton.js
@@ -1,7 +1,68 @@
 import React, { Component } from "react";
-
+import styles from "./BorderdButton.module.css";
+import PropTypes from "prop-types";
 export default class BorderdButton extends Component {
+  handleClick = () => {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  };
   render() {
-    return <div></div>;
+    return (
+      <div className={styles.buttonBase}>
+        <div
+          className={styles.buttonHolder}
+          style={{
+            padding: this.props.padding,
+            height: ` ${this.props.height}px`,
+            width: `${this.props.width}px`,
+            borderRadius: this.props.borderRadius,
+            backgroundColor: this.props.backgroundColor,
+            border: `1px solid ${
+              this.props.borderColor
+                ? this.props.borderColor
+                : this.props.fontcolor
+            }`
+          }}
+          onClick={() => this.handleClick()}
+        >
+          <div
+            className={styles.buttonText}
+            style={{
+              color: this.props.fontcolor,
+              fontSize: this.props.fontSize,
+              fontFamily: this.props.fontFamily
+            }}
+          >
+            {this.props.name}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
+BorderdButton.propTypes = {
+  height: PropTypes.number,
+  width: PropTypes.number,
+  backgroundColor: PropTypes.string,
+  name: PropTypes.string,
+  hoverBackgroundColor: PropTypes.string,
+  disabled: PropTypes.bool,
+  borderColor: PropTypes.string,
+  borderRadius: PropTypes.number,
+  fontcolor: PropTypes.string,
+  fontSize: PropTypes.number,
+  fontFamily: PropTypes.string
+};
+
+BorderdButton.defaultProps = {
+  height: 40,
+  backgroundColor: "#1cc7d0",
+  disabled: false,
+  loading: false,
+  iconHeight: 40,
+  iconWidth: 40,
+  fontcolor: "#212121",
+  fontSize: 14,
+  fontFamily: "ubuntu"
+};
